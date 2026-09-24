8d07046d827067c522a6e28b5b43e1c75cbd8ae5
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-09-24 23:12:27 +0900

    add version flag

--- cmd/tailctl/main.go
+++ cmd/tailctl/main.go
@@ -8,14 +8,30 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime/debug"
 
 	"github.com/johejo/tailctl/internal/cli"
 )
 
+// version can be set at build time with -ldflags "-X main.version=v1.2.3".
+var version string
+
+func buildVersion() string {
+	if version != "" {
+		return version
+	}
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" {
+		return info.Main.Version
+	}
+	return "(unknown)"
+}
+
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
-	if err := cli.NewCommand().ExecuteContext(ctx); err != nil {
+	cmd := cli.NewCommand()
+	cmd.Version = buildVersion()
+	if err := cmd.ExecuteContext(ctx); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
